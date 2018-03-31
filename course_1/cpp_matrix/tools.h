c397888d0b58c7dd2527c11a0eafbe64c60afc1d
Daria Kriukova <kda.dariush@gmail.com>
2018-03-31 20:04:53 +0300

    Create tools.h

---
+++ course_1/cpp_matrix/tools.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+#include <time.h>
+#include <conio.h>
+#include <iomanip>
+#include <algorithm>
+using namespace std;
+double FunU(double *, int);
+void viewV(double*, int);
+void viewM(double**, int);
+void task(double**&, double*&, int);
+void mainCreate(double**&, int);
